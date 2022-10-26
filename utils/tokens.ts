2d9d6131943093e88002a0d146ee37fb04cb44f3
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-10-26 11:59:28 +0800

    create erc20 contract api hook

--- utils/tokens.ts
+++ utils/tokens.ts
@@ -1,3 +1,9 @@
+import { BigNumber } from "ethers";
+
 export function minimalTokenToToken(amount: number): number {
   return amount / 10 ** 18;
+}
+
+export function minimalTokenToTokenBigNumber(amount: BigNumber): BigNumber {
+  return amount.div(BigNumber.from(10).pow(18));
 }
