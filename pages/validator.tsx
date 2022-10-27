1d6c653ed274e693b86533a1a5bef636393a57d5
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-10-27 18:37:13 +0800

    create validator page placeholder

---
+++ pages/validator.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+const ValidatorPage = () => {
+  return (
+    <Layout title="Validators" bannerTitle="Validators">
+      <div className="m-5 lg:m-28 mb-40"></div>{" "}
+    </Layout>
+  );
+};
+export default ValidatorPage;
