bbe3650fdd34ee92ee67fab29826cad93d6cd489
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2024-12-07 18:57:41 -0500

    add https

--- lib/solana/constants.ts
+++ lib/solana/constants.ts
@@ -1,2 +1,2 @@
 export const SOLANA_API_URL =
-  process.env.NEXT_PUBLIC_SOLANA_API_URL ?? "http://solana-api.ivyoracle.xyz";
+  process.env.NEXT_PUBLIC_SOLANA_API_URL ?? "https://solana-api.ivyoracle.xyz";
