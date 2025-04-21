02cd5f6f7158fe47903a3049c0c2ce4bfce0b78d
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2025-04-21 18:16:23 -0400

    Add company name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -128,7 +128,7 @@ function Footer() {
         <div className="flex flex-wrap items-center md:justify-between justify-center">
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
             <div className="text-sm text-gray-600 font-semibold py-1">
-              Copyright © {new Date().getFullYear()}{" "}
+              Copyright © {new Date().getFullYear()}, Ivy Oracle Inc.{" "}
             </div>
           </div>
         </div>
